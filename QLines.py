f4d871d2f525c6b8c55fdb19f1f51d09e819a70e
Fraser McLeod <fdm360@mail.usask.ca>
2023-12-05 20:19:34 -0600

    create Line class and refactoring

--- QLines.py
+++ QLines.py
@@ -2,15 +2,18 @@
 
 
 
-class DCEL:
-    """A doubly-connected edge list (DCEL) is a data structure to represent a subdivision of the plane.
+class LineArrangement:
+    """Represent an arrangement of lines in the plane.
 
+    We choose to represent the subdivision created from lines using a doubly-connected edge list (DCEL).
     The purpose of this data structure is to represent a planar subdivision and efficiently:
         Traverse the boundary of a given face.
         Access one face from an adjacent one.
         Or visit all the edges of a given vertex.
 
     Attributes:
+        lines: a list of lines in the plane. Each line should be represent as a tuple: (x1, y1, x2, y2) where
+               (x1, y1) and (x2, y2) are two distinct points on the plane.
         vertexRecord: a list of dictionaries to represent the vertices of the arrangement. Each dictionary will have:
                         coordinates: tuple (x, y)
                         incidentEdge: reference to an arbitrary half edge with v as its origin
@@ -25,13 +28,114 @@ class DCEL:
                         prev: previous edge on the boundary of incident face
 
     """
-    def __init__(self):
+    def __init__(self, lines: list[tuple]):
+        self.lines = lines
         self.vertexRecord = []
         self.faceRecord = []
         self.edgeRecord = []
+        # create arrangement
 
-    def boundingBox(self, v: tuple):
-        """Compute a bounding box with corner vertex v"""
+    def boundingBox(self, left: float, right: float, top: float, bottom: float):
+        """Compute a bounding box with corners (left, top), (right, top), (right, bottom), (left, bottom)
 
-    def boundingRegion(self, vList: list[tuple]):
-        """Compute the arrangement given the vertices of a polygon"""
+        Args:
+            left: leftmost value of the box
+            right: rightmost value of the box
+            top: topmost value of the box
+            bottom: bottommost value of the box
+        """
+        # only want to compute bounding box if the arrangement is currently null
+        assert len(self.vertexRecord) == 0
+
+        # We will have 4 vertices, 8 half edges, and 2 faces
+        self.vertexRecord.append({"self": 0, "coord": (left, top), "incEdge": 4})
+        self.vertexRecord.append({"self": 1, "coord": (right, top), "incEdge": 5})
+        self.vertexRecord.append({"self": 2, "coord": (right, bottom), "incEdge": 6})
+        self.vertexRecord.append({"self": 3,"coord": (left, bottom), "incEdge": 7})
+
+        # edges going counter clockwise, beginning at (left, top) vertex, left is bounded face
+        self.edgeRecord.append({"self": 0, "origin": 0, "twin": 7, "incFace": 0, "next": 1, "prev": 3}) #0
+        self.edgeRecord.append({"self": 1, "origin": 3, "twin": 6, "incFace": 0, "next": 2, "prev": 0}) #1
+        self.edgeRecord.append({"self": 2, "origin": 2, "twin": 5, "incFace": 0, "next": 3, "prev": 1}) #2
+        self.edgeRecord.append({"self": 3, "origin": 1, "twin": 4, "incFace": 0, "next": 0, "prev": 2}) #3
+
+        # edges going clockwise, beginning at (left, top) vertex, left is unbounded face
+        self.edgeRecord.append({"self": 4, "origin": 0, "twin": 3, "incFace": 1, "next": 5, "prev": 3}) #4
+        self.edgeRecord.append({"self": 5, "origin": 1, "twin": 2, "incFace": 1, "next": 6, "prev": 4}) #5
+        self.edgeRecord.append({"self": 6, "origin": 2, "twin": 1, "incFace": 1, "next": 7, "prev": 5}) #6
+        self.edgeRecord.append({"self": 7, "origin": 3, "twin": 0, "incFace": 1, "next": 8, "prev": 6}) #7
+
+        # 0 is the bounded face and 1 is the unbounded face
+        self.faceRecord.append({"self": 0, "outComp": 4, "inComp": []})
+        self.faceRecord.append({"self": 1, "outComp": None, "inComp:": [4]})
+
+    def lineArrangement(self):
+        """Compute the arrangement of lines on the plane and store in DCEL
+
+        Args:
+            lines: a list of lines represented by a 4-tuple: (x1, y1, x2, y2) of two points on the line
+        """
+        # begin by computing the bounding box
+
+
+    def findExtreme(self) -> tuple:
+        """Find the leftmost, rightmost, topmost, and bottommost intersection point in self.lines
+
+        return:
+            a tuple (left, right, top, bottom) of integers representing the most extreme values
+            of the intersection points
+        """
+        l1 = self.lines[0]
+        l2 = self.lines[1]
+
+    def findIntersection(self, l1: tuple, l2: tuple):
+
+
+class Line():
+    """Represent a line
+
+    Attributes:
+        p1: an arbitrary point (x1, y1)
+        p2: a second distinct arbitrary point (x2, y2)
+        slope: the slope of the line
+        xInt: the x-intercept
+        yInt: the y-intercept
+    """
+
+    def __init__(self, p1: tuple, p2:tuple):
+        self.p1 = p1
+        self.p2 = p2
+        self.slope = (p2[1]-p1[1])/(p2[1]-p2[0])
+        self.yInt = self.p1[1] - self.slope*p1[0]
+        self.xInt = self.yInt/(-self.slope)
+
+    def intercept(self, l: Line) -> tuple:
+        """Return the interscetion between self and l. If parrallel return None
+        """
+        if (self.slope == l.slope):
+            return None
+
+        x = (l.yInt - self.yInt)/(self.slope-l.slope)
+        y = self.slope*x + self.yInt
+
+        return (x, y)
+
+
+
+    def x1(self):
+        return self.p1[0]
+
+    def y1(self):
+        return self.p1[1]
+
+    def x2(self):
+        return self.p2[0]
+
+    def y2(self):
+        return self.p2[1]
+
+    def slope(self):
+        return self.slope
+
+    def yInt(self):
+        return self.yInt
